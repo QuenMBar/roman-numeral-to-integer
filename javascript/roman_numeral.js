7fad05a6f14ae0adb0fe713a7de8c220343dd2a9
QuenMBar <Qbeast360@gmail.com>
2021-03-24 13:59:37 -0700

    Done.

--- javascript/roman_numeral.js
+++ javascript/roman_numeral.js
@@ -1,21 +1,44 @@
 function romanNumeral(string) {
-  // type your code here
+    let value = 0;
+    const order = ["M", "D", "C", "L", "X", "V", "I"];
+    const key = {
+        M: 1000,
+        D: 500,
+        C: 100,
+        L: 50,
+        X: 10,
+        V: 5,
+        I: 1,
+    };
+
+    for (let i = 0; i < string.length; i++) {
+        value += key[string.charAt(i)];
+        if (i !== 0) {
+            if (
+                string.charAt(i - 1) === order[order.indexOf(string.charAt(i)) + 1] ||
+                string.charAt(i - 1) === order[order.indexOf(string.charAt(i)) + 2]
+            ) {
+                value -= key[string.charAt(i - 1)] * 2;
+            }
+        }
+    }
+    return value;
 }
 
 if (require.main === module) {
-  // add your own tests in here
-  console.log("Expecting: 1");
-  console.log(romanNumeral('I'));
+    // add your own tests in here
+    console.log("Expecting: 1");
+    console.log(romanNumeral("I"));
 
-  console.log("");
+    console.log("");
 
-  console.log("Expecting: 9");
-  console.log(romanNumeral('IX'));
+    console.log("Expecting: 9");
+    console.log(romanNumeral("IX"));
 
-  console.log("");
+    console.log("");
 
-  console.log("Expecting: 402");
-  console.log(romanNumeral('CDII'));
+    console.log("Expecting: 402");
+    console.log(romanNumeral("CDII"));
 }
 
 module.exports = romanNumeral;
